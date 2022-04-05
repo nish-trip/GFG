067bf4940841a000a24f259a8b65f723ce0644d3
nishant tripathy <ntripathy@MacBook-Pro.local>
2022-04-05 15:38:25 +0530

    adding header file

---
+++ test.h
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+#include<map>
+#include<deque>
+#include<queue>
+#include<stack>
+#include<unordered_set>
+#include<set>
+#include<list>
+
+using namespace std;
